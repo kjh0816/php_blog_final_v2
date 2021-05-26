893262e279d3a423721e428aa18f7ba61b3f6482
jhs512 <jangka512@gmail.com>
2021-05-26 12:30:16 +0900

    공통 상단, 하단 디자인 적용

--- usr/article/modify.php
+++ usr/article/modify.php
@@ -21,41 +21,33 @@ if ( $article == null ) {
   exit;
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>게시물 수정, <?=$id?>번 게시물</title>
-</head>
-<body>
-  <h1>게시물 수정, <?=$id?>번 게시물</h1>
-  <hr>
-  <div>
-    <a href="list.php">글 리스트</a>
-    <a href="detail.php?id=<?=$id?>">원문</a>
-  </div>
-  <hr>
+<?php
+$pageTitle = "게시물 수정, ${id}번 게시물";
+?>
+<?php require_once __DIR__ . "/../head.php"; ?>
+<div>
+  <a href="list.php">글 리스트</a>
+  <a href="detail.php?id=<?=$id?>">원문</a>
+</div>
+<hr>
 
-  <form action="doModify.php">
-  <input type="hidden" name="id" value="<?=$article['id']?>"> 
+<form action="doModify.php">
+<input type="hidden" name="id" value="<?=$article['id']?>"> 
+<div>
+    <span>번호</span>
+    <span><?=$article['id']?></span>
+  </div>
   <div>
-      <span>번호</span>
-      <span><?=$article['id']?></span>
-    </div>
-    <div>
-      <span>제목</span>
-      <input required placeholder="제목을 입력해주세요." type="text" name="title" value="<?=$article['title']?>"> 
-    </div>
-    <div>
-      <span>내용</span>
-      <textarea required placeholder="내용을 입력해주세요." name="body"><?=$article['body']?></textarea>
-    </div>
-    <div>
-      <input type="submit" value="글수정">
-    </div>
-  </form>
-  
-</body>
-</html>
+    <span>제목</span>
+    <input required placeholder="제목을 입력해주세요." type="text" name="title" value="<?=$article['title']?>"> 
+  </div>
+  <div>
+    <span>내용</span>
+    <textarea required placeholder="내용을 입력해주세요." name="body"><?=$article['body']?></textarea>
+  </div>
+  <div>
+    <input type="submit" value="글수정">
+  </div>
+</form>
+
+<?php require_once __DIR__ . "/../foot.php"; ?>
