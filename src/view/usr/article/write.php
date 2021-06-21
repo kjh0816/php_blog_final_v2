19f249b10c563c84c2924044db75a869f75eb7eb
jhs512 <jangka512@gmail.com>
2021-06-21 10:46:24 +0900

    글 작성시 폼체크

--- src/view/usr/article/write.php
+++ src/view/usr/article/write.php
@@ -7,14 +7,41 @@ $pageTitle = "게시물 작성";
 <section class="secion-article-write">
   <div class="container mx-auto">
     <div class="con-pad">
-      <form action="doWrite" method="POST">
+      <script>
+      let ArticleDoWrite__submitFormDone = false;
+      function ArticleDoWrite__submitForm(form) {
+        if ( ArticleDoWrite__submitFormDone ) {
+          return;
+        }
+
+        form.title.value = form.title.value.trim();
+
+        if ( form.title.value.length == 0 ) {
+          alert('제목을 입력해주세요.');
+          form.title.focus();
+
+          return;
+        }
+
+        if ( form.body.value.length == 0 ) {
+          alert('내용을 입력해주세요.');
+          form.body.focus();
+
+          return;
+        }
+
+        form.submit();
+        ArticleDoWrite__submitFormDone = true;
+      }
+      </script>
+      <form action="doWrite" method="POST" onsubmit="ArticleDoWrite__submitForm(this); return false;">
         <div>
           <span>제목</span>
-          <input required placeholder="제목을 입력해주세요." type="text" name="title"> 
+          <input placeholder="제목을 입력해주세요." type="text" name="title"> 
         </div>
         <div>
           <span>내용</span>
-          <textarea class="w-full p-4 h-96" required placeholder="내용을 입력해주세요." name="body"></textarea>
+          <textarea class="w-full p-4 h-96" placeholder="내용을 입력해주세요." name="body"></textarea>
         </div>
         <div>
           <input type="submit" value="글작성">
